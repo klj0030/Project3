1e9c5a47d5e3e0b6a27ccd6f4d71788d688cb0f3
klj0030 <54611753+klj0030@users.noreply.github.com>
2019-12-08 04:04:25 -0600

    Add files via upload

---
+++ CustomerModel.java
@@ -0,0 +1,13 @@
+public class CustomerModel {
+    public int mCustomerID;
+    public String mName, mAddress, mEmail;
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder("(");
+        sb.append(mCustomerID).append(",");
+        sb.append("\"").append(mName).append("\"").append(",");
+        sb.append("\"").append(mAddress).append("\"").append(",");
+        sb.append("\"").append(mEmail).append("\"").append(")");
+        return sb.toString();
+    }
+}
---
+++ PurchaseListModel.java
@@ -0,0 +1,7 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PurchaseListModel {
+    public List<PurchaseModel> purchases = new ArrayList<>();
+}
